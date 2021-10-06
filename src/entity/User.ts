58b2d4dcd0a830964c7a6ff286b8dabb43b1939e
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-10-05 18:00:45 -0600

    Refactoring de User Entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,33 @@
+import { IsDate, IsEmail, IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
 import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
 @Entity()
 export class User {
 
+    @IsInt()
     @PrimaryGeneratedColumn()
     id: number;
 
+    @IsNotEmpty()
     @Column()
     firstName: string;
 
+    @IsNotEmpty()
     @Column()
     lastName: string;
 
+    @IsEmail()
+    @IsNotEmpty()
+    @Column({unique: true})
+    email: string;
+
+    @IsString()
+    @MinLength(8)
     @Column()
-    age: number;
+    password: string;
+
+    @IsDate()
+    @Column()
+    birthay: Date;
 
 }
