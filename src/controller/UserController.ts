5bca5239ec5daed58f84d546f1dbba61bdf99e5a
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-10-05 20:20:02 -0600

    Se agregan acciones basicas para Controlador de User

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,36 @@
 import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
+import {Request, Response} from "express";
 import {User} from "../entity/User";
 
 export class UserController {
 
-    private userRepository = getRepository(User);
-
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+    static all = async (request: Request, response: Response) => {
+        const userRepository = getRepository(User);
+        const users = await userRepository.find();
+        
+        response.status(200).json(users);
     }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
+    static one = async (request: Request, response: Response) => {
+        const userRepository = getRepository(User);
+        const user = await userRepository.findOneOrFail(request.params.id);
+        
+        response.status(200).json(user);
     }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+    static save = async (request: Request, response: Response) => {
+        const userRepository = getRepository(User);
+        const saved = await userRepository.save(request.body);
+
+        response.status(201).json(saved);
     }
 
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+    static remove = async (request: Request, response: Response) => {
+        const userRepository = getRepository(User);
+        let userToRemove = await userRepository.findOne(request.params.id);
+        const removed = await userRepository.remove(userToRemove);
+
+        response.status(200).json(removed);
     }
 
 }
--- src/routes/UsersRouter.ts
+++ src/routes/UsersRouter.ts
@@ -1,14 +1,13 @@
-import { Router, response, request } from "express";
+import { Router } from "express";
+import { UserController } from "../controller/UserController";
 
 const USER_ROUTER = Router();
 
-USER_ROUTER.get("/", (request, response) => {
-    response.json("It works!");
-});
+USER_ROUTER.get("/", UserController.all);
 
-USER_ROUTER.get("/:id");
-USER_ROUTER.post("/:id");
+USER_ROUTER.get("/:id", UserController.one);
+USER_ROUTER.post("/:id", UserController.save);
 USER_ROUTER.put("/:id");
-USER_ROUTER.delete("/:id");
+USER_ROUTER.delete("/:id", UserController.remove);
 
 export default USER_ROUTER;
